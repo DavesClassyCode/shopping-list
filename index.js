bc7c72fb7871e1231f56bc1612c18992832af93f
DavesClassyCode <davidrnowosad@gmail.com>
2022-08-16 13:57:08 -0400

    addItem functionality created

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 'use strict';
 
-function addItem(){
+function addItem(newItem) {
+    $('.shopping-list').append(`
+            <li>
+            <span class="shopping-item">${newItem.val()}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+            </button>
+            </div>
+        </li>
+    `);
+    console.log('addItem() ran');
+}
+
+function checkItem() {
 
 }
 
-function checkItem(){
-    
+function deleteItem() {
+
 }
 
-function deleteItem(){
-    
-}
+function shoppingList() {
+    //Add Item
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        const newItem = $(event.currentTarget).find('#shopping-list-entry');
+        console.log('shoppingList() ran');
+        addItem(newItem);
+        newItem.val('');
+    });
 
-function shoppingList(){
 
+    console.log('shoppingList() ran');
 }
 
 $(shoppingList);
